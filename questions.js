f0f510fb26b9df3a104b9bd6d13ac90b9c3816ae
garrett krage <garrett.krage@gmail.com>
2019-12-17 22:28:29 -0700

    added to the js file. added a for loop to update the buttons and text field with respective answers and question.'

--- questions.js
+++ questions.js
@@ -14,3 +14,14 @@ var questions = [
 
 
 
+$('.card-body').text(questions[0].title);
+
+var btnChild = 1;
+for (i = 0; i < $('.buttons').children().length; i++){
+
+    index = $('.buttons').children().get(i)
+    $(index).attr('data-number', btnChild)
+    $(index).text(questions[0].choices[i])
+    
+    btnChild++
+}
