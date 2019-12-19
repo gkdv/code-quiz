f5d8e5bacd4d07b5f2db4ba8a4a42bbf0b8bdbfc
garrett krage <garrett.krage@gmail.com>
2019-12-18 21:41:40 -0700

    created timer functionality.

--- questions.js
+++ questions.js
@@ -11,17 +11,3 @@ var questions = [
     },
     ///etc.
 ];
-
-
-
-$('.card-body').text(questions[0].title);
-
-var btnChild = 1;
-for (i = 0; i < $('.buttons').children().length; i++){
-
-    index = $('.buttons').children().get(i)
-    $(index).attr('data-number', btnChild)
-    $(index).text(questions[0].choices[i])
-    
-    btnChild++
-}
--- script.js
+++ script.js
@@ -1,13 +1,57 @@
 
 
-$('.card-body').text(questions[0].title);
-
 var btnChild = 1;
-for (i = 0; i < $('.buttons').children().length; i++){
-
+for (i = 0; i < $('.buttons').children().length; i++) {
+    
     index = $('.buttons').children().get(i)
     $(index).attr('data-number', btnChild)
-    $(index).text(questions[0].choices[i])
-    
     btnChild++
 }
+
+$('#startGameBtn').on('click', function () {
+    $('#startGameBtn').hide();
+    //start timer
+    setTime();
+    //pull up first question
+})
+
+
+var secondsLeft = 60;
+
+function setTime() {
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        $('#countDown').text('Timer: ' + secondsLeft);
+
+        if (secondsLeft === 0) {
+            clearInterval(timerInterval);
+            alert('quiz over'); //add send message function here when it is ready
+        }
+
+    }, 1000);
+}
+
+// function sendMessage() {
+//     timeEl.textContent = " ";
+
+//     var imgEl = document.createElement("img");
+
+//     imgEl.setAttribute("src", "images/image_1.jpg");
+//     mainEl.appendChild(imgEl);
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $('.card-body').text(questions[0].title);
+// $(index).text(questions[0].choices[i])
